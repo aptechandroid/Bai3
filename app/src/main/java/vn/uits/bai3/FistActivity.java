95bb61a0612b57cec778fc200d39d2d1d0a858fa
phuquy2114 <phuquycntt@gmail.com>
2018-03-13 21:11:54 +0700

    Android Camera

--- app/src/main/java/vn/uits/bai3/FistActivity.java
+++ app/src/main/java/vn/uits/bai3/FistActivity.java
@@ -1,10 +1,14 @@
 package vn.uits.bai3;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Copyright © 2017 BAP CO., LTD
@@ -13,10 +17,17 @@ import android.view.View;
 
 public class FistActivity extends AppCompatActivity {
 
+    private static final int REQUEST_TAKE_PHOTO = 100;
+    private static final int REQUEST_CROP_PHOTO = 101;
+    private Uri mUri;
+
+    private ImageView mImgThumbnail;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fist);
+        mImgThumbnail = findViewById(R.id.mImgThumbnail);
     }
 
     public void onViewPager(View view) {
@@ -32,5 +43,42 @@ public class FistActivity extends AppCompatActivity {
     public void onMediaPlayer(View view) {
         Intent media = new Intent(this, MediaPlayerActivity.class);
         startActivity(media);
+    }
+
+    public void onTakePhoto(View view) {
+        Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePhoto, REQUEST_TAKE_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO) {
+            mUri = data.getData();
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImgThumbnail.setImageBitmap(imageBitmap);
+        } else if (requestCode == REQUEST_CROP_PHOTO) {
+            mUri = data.getData();
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImgThumbnail.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void onCropImage() {
+        Intent copImage = new Intent("com.android.camera.action.CROP");
+        copImage.setDataAndType(mUri, "image/*");
+        copImage.putExtra("crop", true);
+
+        copImage.putExtra("aspectX", 1);
+        copImage.putExtra("aspectY", 1);
+
+        copImage.putExtra("outputX", 300);
+        copImage.putExtra("outputY", 300);
+
+        copImage.putExtra("return-data", true);
+
+        startActivityForResult(copImage, REQUEST_CROP_PHOTO);
     }
 }
